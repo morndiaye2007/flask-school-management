52068340035e4508ff9d2ebe3335a7f04482cd3b
Mor Ndiaye <morn5574@gmail.com>
2025-03-09 09:07:21 +0000

    autre des commit

--- tests/test_models.py
+++ tests/test_models.py
@@ -29,17 +29,6 @@ class TestModels(unittest.TestCase):
         self.assertEqual(saved_student.age, 18)
         self.assertEqual(saved_student.grade, 'Terminale')
         
-    def test_teacher_model(self):
-        # Test de création d'un professeur
-        teacher = Teacher(name='Marie Martin', subject='Mathématiques')
-        db.session.add(teacher)
-        db.session.commit()
-        
-        # Vérifier que le professeur a été créé correctement
-        saved_teacher = Teacher.query.filter_by(name='Marie Martin').first()
-        self.assertIsNotNone(saved_teacher)
-        self.assertEqual(saved_teacher.subject, 'Mathématiques')
-        
     def test_course_model(self):
         # Test de création d'un cours
         course = Course(name='Algèbre linéaire', description='Étude des espaces vectoriels')
@@ -62,28 +51,4 @@ class TestModels(unittest.TestCase):
         self.assertIsNotNone(saved_classroom)
         self.assertEqual(saved_classroom.capacity, 30)
         
-    def test_schedule_model(self):
-        # Créer les objets nécessaires
-        course = Course(name='Algèbre linéaire', description='Étude des espaces vectoriels')
-        teacher = Teacher(name='Marie Martin', subject='Mathématiques')
-        classroom = Classroom(name='Salle 101', capacity=30)
-        
-        db.session.add_all([course, teacher, classroom])
-        db.session.commit()
-        
-        # Test de création d'un emploi du temps
-        schedule = Schedule(
-            course_id=course.id,
-            teacher_id=teacher.id,
-            classroom_id=classroom.id,
-            time='Lundi 10h-12h'
-        )
-        db.session.add(schedule)
-        db.session.commit()
-        
-        # Vérifier que l'emploi du temps a été créé correctement
-        saved_schedule = Schedule.query.filter_by(time='Lundi 10h-12h').first()
-        self.assertIsNotNone(saved_schedule)
-        self.assertEqual(saved_schedule.course_id, course.id)
-        self.assertEqual(saved_schedule.teacher_id, teacher.id)
-        self.assertEqual(saved_schedule.classroom_id, classroom.id)
+       
